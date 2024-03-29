78cd24390f7d37da4fe0790f2dd5ca86a6ffcd3e
luke <ivan1.krasikov.2003@gmail.com>
2024-03-29 11:03:30 +0300

    change structure project

--- app/index.php
+++ app/index.php
@@ -1,89 +1,14 @@
-<?php
-namespace main;
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Лабораторные работы по PHP</title>
+</head>
+<body>
+    <a href="lab1_2/lab1_2.php">Задание 1 и 2</a>
 
-include 'testing_system/Exam.php';
-include 'problems/Solution.php';
-
-use problems\Solution;
-use testing_system\Exam;
-
-
-ini_set('display_errors', 'Off');
-function printResult($zadacha)
-{
-    $output = '';
-    foreach ($zadacha as $key => $result) {
-        $n = $key + 1;
-        $mark = $result ? ' &#10003' : '&#10007';
-        $output .= "<div>Тест $n : $mark</div>";
-    }
-    echo $output;
-}
-
-$input1 = '';
-if($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['input1']){
-    $input1 = $_POST['input1'];
-}
-
-$input2 = '';
-if($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['input2']){
-    $input2 = $_POST['input2'];
-}
-
-
-$ex1 = new Exam('A');
-$zadacha1 = $ex1->examTests('problems\Solution::threeSharpAxes');
-
-$ex2 = new Exam('B');
-$zadacha2 = $ex2->examTests('problems\Solution::sizeMatters');
-
-$ex3 = new Exam('C');
-$zadacha3 = $ex3->examTests('problems\Solution::cutOnce');
-
-?>
-
-<div style="display: flex;
-flex-direction: row;
-justify-content: space-around;
-margin-bottom: 3rem;">
-    <div>
-        <div>Три острых топора</div>
-        <?php printResult($zadacha1); ?>
-    </div>
-    <div>
-        <div>Размер имеет значение</div>
-        <?php printResult($zadacha2); ?>
-    </div>
-    <div>
-        <div>Семь раз отмерь, один раз отрежь</div>
-        <?php printResult($zadacha3); ?>
-    </div>
-</div>
-<div style="margin-bottom: 3rem;">
-    <div>Задача 1 на регулярки</div>
-    <form action="index.php" method="post">
-        <input type="text" name="input1" value="<?= $input1 ?>">
-        <input type="submit">
-    </form>
-
-    <?php if($input1) {?>
-        <div>Ответ: <?= Solution::regProblem1($input1) ?></div>
-    <?php } ?>
-</div>
-<div style="margin-bottom: 3rem;">
-    <div>Задача 2 на регулярки</div>
-    <form action="index.php" method="post">
-        <input style="width: 100%" type="text" name="input2" value="<?= $input2 ?>">
-        <input type="submit">
-    </form>
-
-    <?php if($input2) {?>
-        <div>Ответ: <?= Solution::regProblem2($input2) ?></div>
-    <?php } ?>
-</div>
-<!--<div>Интерактивное тестирование</div>-->
-<?php
-//echo '<pre>';
-//$ex3->examIter('problems\Solution::cutOnce', 0);
-//echo '</pre>';
-//?>
+</body>
+</html>
