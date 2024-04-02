a38dc145d26dd2db2e8fb8656af0c7a89cbf344a
luke <ivan1.krasikov.2003@gmail.com>
2024-04-02 10:55:26 +0300

    add comments

--- app/lab1_2/problems/Solution.php
+++ app/lab1_2/problems/Solution.php
@@ -8,30 +8,45 @@ class Solution
     public static function threeSharpAxes($input) : int
     {
         $inputArray = [];
+        // Сплитим строку по \n
         $tmpArray = explode("\n", $input);
+        // Проходимся по всем значениям нового массива
         foreach ($tmpArray as $item) {
+            // Проверям если в строке 3 элемента, значит это ставка
             if(count(explode(" ", $item)) == 3){
+                // Делим массив на номер матча, сумму ставки и на результат матча
                 list($n, $total, $result) = explode(" ", $item);
                 $bet = compact(array('n', 'total', 'result'));
+                // Помещаем в подмассив ставок
                 $inputArray['bets'][] = $bet;
                 continue;
             }
+            // Проверям если в строке 5 элемента, значит это матч
             if(count(explode(" ", $item)) == 5){
+                // Разбиваем строку на номер матча, коэффициенты на комманд и на результат
                 list($n, $L, $R, $D, $result) = explode(" ", $item);
                 $match = compact(array('n', 'L', 'R', 'D', 'result'));
+                // Помещаем в подмассив матчей
                 $inputArray['matches'][] = $match;
                 continue;
             }
             $inputArray[] = explode(" ", $item);
         }
 
+        // Общая сумма ставок
         $overTotalBets = 0;
+        // Выигрыш со ставок
         $totalWin = 0;
 
+        // Проходимся по ставкам
         foreach ($inputArray['bets'] as $bet) {
+            // Добавляем сумму ставки в общую сумму ставок
             $overTotalBets += (int)($bet['total']);
+            // Матч на который было поставлено
             $match = $inputArray['matches'][(int)$bet['n'] - 1];
+            // Проверям зашла ли ставка
             if ($bet['result'] == $match['result']) {
+                // Добавляем в сумму выигрыша произведение коэффицинта и суммы ставки
                 $totalWin += (int)$bet['total'] * (float)$match[$bet['result']];
             }
         }
@@ -44,14 +59,21 @@ class Solution
     {
         $inputArray = [];
         $tmpArray = explode("\n", $input);
+        // Проходимся по входным строкам
         foreach ($tmpArray as $item) {
+            // Сплитим строку на элементы, которые разделенны :
             $item = explode(":", $item);
+            // Удаляем из массива пустые элементы
             $item = array_diff($item, array(''));
             $item = array_map(function ($value)
             {
+                // Если длина элемента массива меньше 4, то мы добавляем к нему 0
                 if (strlen($value) < 4) {
+                    // Сплитим строку на символы
                     $value = str_split($value);
+                    // Создаем массив с недостающими нулями в элементе массива
                     $zeroArray = array_fill(0, 4 - count($value), 0);
+                    // Сливаем массив нулей и массив с нашими значениями, новый массив объединяем в строку
                     return implode('', array_merge($zeroArray, $value));
                 }
                 return $value;
@@ -59,19 +81,26 @@ class Solution
             $inputArray[] = $item;
         }
 
+        //  Проходимся по нашим массивам и добиваем их недостающими 0000
         foreach ($inputArray as $key => $item) {
+            // Если длина массива меньше 8, значит в нем не достает 0000
             if(count($item) < 8){
+                // Определяем какое количество 0000 нам нужно добить в массив
                 $tmp = 8 - count($item);
                 for ($i = 0; $i < 8; $i++) {
+                    // Проверяем существует ли данный ключ в массиве, если нет значит нам нужно поместить туда 0000
                     if (!key_exists($i, $item)){
                         $res = [];
+                        // Создаем массив из 0000 размера недостающего количества
                         for ($j = 0; $j < $tmp; $j++) {
                             $res[] = '0000';
                         }
+                        // Помещаем массив из 0000 в массив с нашими значениями
                         array_splice($item, $i, 0, $res);
                     }
                 }
             }
+            // Соединяем массив в строку
             $inputArray[$key] = implode(':', $item);
         }
         $output = implode("\n", $inputArray);
@@ -81,33 +110,43 @@ class Solution
     // Задача "Семь раз отмерь, один раз отрежь"
     public static function cutOnce($input) : string
     {
+        // Разбиваем входные строки на значение и параметр валидации
         $inputArray = array_map(function ($v){
             return preg_split("/(?<=\>)\s/", $v);
         }, explode("\n", $input));
 
         $result = array_map(function ($v) {
             switch ($v[1][0]) {
+                // Валидация для обычной строки
                 case 'S':
                     $params = explode(" ", $v[1]);
+                    // Проверяем на длину
                     return strlen(trim($v[0], "<>")) <= (int)$params[2] &&
                         strlen(trim($v[0], "<>")) >= (int)$params[1] ?
                         "OK" : "FAIL";
+                // Валидация для числа
                 case 'N':
                     $params = explode(" ", $v[1]);
                     $str = trim($v[0], "<>");
+                    // Провеяем являются ли данные числом, в проверку также входит проверка на отрицательные числа
                     if(preg_match("#^(-\d+)$|^(\d+)$#", $str)){
                         return (int)$str <= (int)$params[2] &&
                         (int)$str >= (int)$params[1] ?
                             "OK" : "FAIL";
                     }
                     return "FAIL";
+                // Валидация номера телефона
                 case 'P':
                     return preg_match('#^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$#', trim($v[0], "<>")) ?
                         "OK" : "FAIL";
+                // Валидация даты
                 case 'D':
                     $matches = [];
                     if(preg_match('#^([0-9]|[0-2][0-9]|[3][0-1]).([0][0-9]|[1][0-1]|[0-9]|12).(\d{4}) ([0-1][0-9]|[2][0-3]|[0-9]):([0-5][0-9])$#',
                         trim($v[0], "<>"), $matches)) {
+                        // В массив matches помещаются подгруппы регулярного выражения, в подгруппе 2 находится месяц
+                        // В подгруппе 1 день месяца, а в подгруппе 3 год
+                        // Проверка на дни февраля в високосный год и в не високосный
                         if((int)$matches[2] == 2){
                             if ((int)$matches[1] <= 28 && (int)$matches[3] % 4 != 0) return "OK";
                             if ((int)$matches[1] <= 29 && (int)$matches[3] % 4 == 0) return "OK";
@@ -116,6 +155,7 @@ class Solution
                         return "OK";
                     }
                     return "FAIL";
+                // Валидация электронной почты
                 case 'E':
                     return preg_match('#^(^[a-zA-Z0-9][a-zA-Z0-9_]{3,29})@([a-zA-Z]{2,30})\.([a-z]{2,10})$#',
                         trim($v[0], "<>")) ?
@@ -138,6 +178,7 @@ class Solution
     // Задача 1 на регулярки
     public static function regProblem1($input) : string
     {
+        // Находим число в строке и заменяем его на произведение этого числа и 2
         return preg_replace_callback("#'\d+'#", function ($matches){
             $v = trim($matches[0], "'");
             return "'" . (int)$v*2 . "'";
@@ -147,7 +188,9 @@ class Solution
     // Задача 2 на регулярки
     public static function regProblem2($input) : string
     {
+        // Находим в строке параметр url
         preg_match("#(?<=&RN=)(\d+-\d+)(?=&)#", $input, $matches);
+        // И помещаем этот параметр в новый url
         return "http://sozd.parlament.gov.ru/bill/$matches[0]";
     }
 }
--- app/lab1_2/testing_system/Exam.php
+++ app/lab1_2/testing_system/Exam.php
@@ -4,22 +4,29 @@ namespace testing_system;
 
 class Exam
 {
+    // Свойство класса, указывающее на дерикторию теста
     private $dirTests;
+    // Ассоциативный массив с тестами
     private $tests;
 
+    // При создании класса указываем название директории с тестами
     public function __construct($dirTests)
     {
         $this->dirTests = "tests/$dirTests";
+        // Создаемый массив
         $this->tests = [
             'dat' => [],
             'ans' => []
         ];
     }
 
+    // Метод для инициализации тестов
     private function initTests()
     {
+        // Получаем в массив имена файлов с тестами
         $files = scandir($this->dirTests);
         unset($files[0], $files[1]);
+        // Проходимся по всем файлам и помещаем их содержимое в массив с тестами
         foreach ($files as $file) {
             $content = "$this->dirTests/$file";
             if (str_ends_with($file, 'dat')) {
@@ -30,6 +37,8 @@ class Exam
         }
     }
 
+    // Метод для проверки одного теста
+    // На вход передаем функцию для теста и номер теста
     public function examIter($callback, $numberTest)
     {
         $this->initTests();
@@ -38,12 +47,18 @@ class Exam
             '<br><br>Правильно' : '<br><br>Не правильно');
     }
 
+    // Метод для проверки функции на всех тестах
     public function examTests($callback)
     {
+        // Инициализируем тесты
         $this->initTests();
+        // Создаем массив, в который будем помещать буллевые значения
         $result = [];
         for ($i = 0; $i < count($this->tests['dat']); $i++){
+            // Сравниваем ответ из теста и значение которое возвращает функция с данными на вход из теста
+            // После чего помещаем результат в массив результата
             $result[] = $this->tests['ans'][$i]  == call_user_func($callback, $this->tests['dat'][$i]);
+            // Если какой то из тестов оказался не верным выводим его номер и результат функции
             if(!($this->tests['ans'][$i]  == call_user_func($callback, $this->tests['dat'][$i]))){
                 print_r("<pre>" . "$i<br>" . call_user_func($callback, $this->tests['dat'][$i]) . "</pre>");
             }
