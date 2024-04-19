8cc528f28c0543f7ee869bc54c9a781fa9fce78c
luke <ivan1.krasikov.2003@gmail.com>
2024-04-19 08:56:21 +0300

    .

--- app/lab3/php/settings-mail.php
+++ app/lab3/php/settings-mail.php
@@ -8,11 +8,7 @@ return [
         'port' => 465,
         'secure' => 'ssl',
         'username' => 'mmiamoto284@gmail.com',
-<<<<<<< HEAD
-        'password' => 'qfpmdyljanxlfdml',
-=======
         'password' => explode("=", file_get_contents("../.env"))[1],
->>>>>>> refs/remotes/origin/main
         'charset' => 'UTF-8',
         'from_email' => 'mmiamoto284@gmail.com',
         'from_name' => 'Ivan',
